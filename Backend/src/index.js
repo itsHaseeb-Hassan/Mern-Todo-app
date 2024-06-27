6d7398548a4d7ed1ee14ed62a10019da5e0b35c3
haseeb hassan <its.haseebhassan07@gmail.com>
2024-06-27 08:58:45 +0500

    frontend added

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -2,17 +2,18 @@ import express from 'express'
 import { configDotenv } from 'dotenv'
 import userRouter from './routes/userRoute.js'
 import todoRouter from './routes/todoRoute.js'
-import { authMidelware } from './midelwares/authMidelware.js'
 import connectDB from './config/db/db.js'
+import cors from 'cors'
 
 configDotenv()
 
 const app=express()
 
 app.use(express.json())
+app.use(cors())
 
 app.use('/api/users',userRouter)
-app.use('/api/todos', authMidelware,todoRouter)
+app.use('/api/todos',todoRouter)
 
 
 
