a2c68834fa448ace245dd3517de0ace13938a35e
haseeb hassan <its.haseebhassan07@gmail.com>
2024-06-27 23:03:08 +0500

    signup screen complete

--- Frontend/todo-frontend/src/pages/SignupScreen.jsx
+++ Frontend/todo-frontend/src/pages/SignupScreen.jsx
@@ -10,13 +10,13 @@ const SignupScreen = () => {
     <div>
           <h1 className='text-center p-9 text-3xl upercase'>Signup User</h1>
             <div className='mx-auto w-[30%] h-[50%] p-4 bg-gray-300 rounded-md backdrop-filter backdrop-blur-md bg-opacity-20'>
-    
+            <FormInput text="Name" type="text" placeholder="Enter your Name" onChange={handleInput}/>
         <FormInput text="Email" type="email" placeholder="Enter your Email" onChange={handleInput}/>
 
         <FormInput text="Password" type="password" placeholder="Enter your Password" onChange={handleInput}/>
         <div className='my-5'>
-        <FormButton text="Login"/>
-        <div className='my-3 text-center'> Do you have an account? <Link to='/login'>Login</Link></div>
+        <FormButton text="Sign Up"/>
+        <div className='my-3 text-center'> Do you have an account? <Link to='/'>Login</Link></div>
         </div>
 </div>
     </div>
